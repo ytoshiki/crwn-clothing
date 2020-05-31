d78bad1893d5f3487000b05a16bac70cb2dd6426
Toshiki <js.nov24@gmail.com>
2020-05-31 10:45:02 +0900

    updating our app with a hp page.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react"
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
 import { Switch, Route } from "react-router-dom"
+import ShopPage from "./components/shop/shop.component"
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
